c0d1011d35b8d29660eb7732cd45b5bd1e739c9f
markpdbailey@gmail.com <markpdbailey@gmail.com@be5b3133-12a5-60eb-c6df-bee0bf063aa0>
2014-03-14 17:53:41 +0000

    wired up buttons

--- src/com/pigdogbay/stopwatch/MainActivity.java
+++ src/com/pigdogbay/stopwatch/MainActivity.java
@@ -3,14 +3,19 @@ package com.pigdogbay.stopwatch;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
 	GameView _GameView;
 	StopWatchGame _StopWatchGame;
+	Button _StartStopBtn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,8 +27,29 @@ public class MainActivity extends Activity {
 		_StopWatchGame = new StopWatchGame();
 		_GameView = new GameView(this, _StopWatchGame);
 		((ViewGroup)findViewById(R.id.surfaceViewContainer)).addView(_GameView);
+		_StartStopBtn = (Button) findViewById(R.id.btnStart);
+		_StartStopBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startStopToggle();
+			}
+		});
+		((Button) findViewById(R.id.btnReset)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				reset();
+			}
+		});
+		
 	}
 
+	private void reset(){
+		
+	}
+	private void startStopToggle(){
+		
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
