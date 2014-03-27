103fbde8fb6e11facb5e0f02a31595b39e11ed61
markpdbailey@gmail.com <markpdbailey@gmail.com@be5b3133-12a5-60eb-c6df-bee0bf063aa0>
2014-03-27 12:07:11 +0000

    Replaced  simpledateformat with timedigits

--- src/com/pigdogbay/stopwatch/StopWatchGame.java
+++ src/com/pigdogbay/stopwatch/StopWatchGame.java
@@ -1,10 +1,5 @@
 package com.pigdogbay.stopwatch;
 
-import java.text.FieldPosition;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import com.pigdogbay.androidutils.games.FrameBuffer;
 import com.pigdogbay.androidutils.games.GameView.IGame;
 
@@ -19,10 +14,7 @@ public class StopWatchGame implements IGame{
 	Model _Model;
 	Bitmap _DigitsBitmap;
 	FrameBuffer _Buffer;
-
-	java.text.SimpleDateFormat _SimpleDateFormat;
-	Date _Date;
-	int _Digits[] = new int[12];
+	TimeDigits _TimeDigits = new TimeDigits();
 
 	public void setDigitsBitmap(Bitmap value){
 		_DigitsBitmap = value;
@@ -30,25 +22,18 @@ public class StopWatchGame implements IGame{
 	
 	public StopWatchGame(Model model){
 		_Model = model;
-		_SimpleDateFormat = new SimpleDateFormat("HH:mm:ss:SSS",Locale.US);
         _Buffer = new FrameBuffer(12*240, 320);
-        _Date = new Date();
 	}
 
 	@Override
 	public void Update() {
-		_Date.setTime(_Model.getElapsedTime());
-		String time = _SimpleDateFormat.format(_Date);
-		for (int i=0;i<12;i++)
-		{
-			_Digits[i] = time.charAt(i)-'0';
-		}
-		
+		_TimeDigits.updateTime(_Model.getElapsedTime());
 	}
 	@Override
 	public void Render(Canvas c) {
+		int[] digits = _TimeDigits.Digits;
 		for (int i=0;i<12;i++){
-			_Buffer.draw(_DigitsBitmap,DIGIT_WIDTH*i,0,_Digits[i]*DIGIT_WIDTH,0,DIGIT_WIDTH, DIGIT_HEIGHT);
+			_Buffer.draw(_DigitsBitmap,DIGIT_WIDTH*i,0,digits[i]*DIGIT_WIDTH,0,DIGIT_WIDTH, DIGIT_HEIGHT);
 		}
 		_Buffer.scaleToFit(c);
 		
